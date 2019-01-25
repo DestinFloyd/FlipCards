f4e0a4e28c2980764af21b817e20ae02b323861f
destin floyd <destin.b.floyd@gmail.com>
2019-01-25 10:31:13 -0500

    ull crud on model 1 and 2 routes set up and checked with postman

--- controllers/qaController.js
+++ controllers/qaController.js
@@ -5,7 +5,6 @@ const QA = require('../models/QA')
 const qaController = {
     index: (req, res) => {
         const setId = req.params.setId
-        
         Set.findById(setId).populate('stack').then(
             (returnedStack) => {
                 const stack = returnedStack.stack
@@ -26,39 +25,29 @@ const qaController = {
                     })
             })
     },
-    // show: (req, res) => {
-    //     const boardID = req.params.boardId
-    //     const userID = req.params.id
-    //     Board.findById(boardID).populate('tasks').then((board) => {
-    //         const tasks = board.tasks
-    //         const name = board.name
-    //         res.render('boards/show', { board: board, name: name, tasks: tasks, userID: userID })
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     })
-    // },
-    // edit: (req, res) => {
-
-    //     const userID = req.params.id
-    //     const boardID = req.params.boardId
-    //     res.render('boards/edit', { userID, boardID })
-    // },
-    // update: (req, res) => {
-    //     const userID = req.params.id
-    //     const boardID = req.params.boardId
-    //     Board.findByIdAndUpdate(boardID, req.body, { new: true })
-    //         .then((board) => {
-    //             res.redirect(`/${userID}/board/${boardID}`)
-    //         })
-    // },
-    // delete: (req, res) => {
-    //     const userID = req.params.id
-    //     const boardId = req.params.boardId
-    //     Board.findByIdAndDelete(boardId)
-    //         .then(() => {
-    //             res.redirect(`/${userID}/board`)
-    //         })
-    // }
+    show: (req, res) => {
+        const setId = req.params.setId
+        const qaId = req.params.qaId
+        QA.findById(qaId).then((qaOne) => {
+            res.send(qaOne)
+        })
+    },
+    update: (req, res) => {
+        const setId = req.params.setId
+        const qaId = req.params.qaId
+        QA.findByIdAndUpdate(qaId, req.body, { new: true })
+            .then((qaOne) => {
+                res.send( qaOne)
+            })
+    },
+    delete: (req, res) => {
+        const setId = req.params.setId
+        const qaId = req.params.qaId
+        QA.findByIdAndDelete(qaId)
+        .then(() => {
+            res.send(200)
+        })   
+    }
 }
 
 module.exports = qaController
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,10 @@ router.delete('/set/:setId', setController.delete)
 router.put('/set/:setId', setController.update)
 
 router.get('/set/:setId/qa', qaController.index)
-// router.get('/set/:setId/qa/:qaId', qaController.show)
+router.get('/set/:setId/qa/:qaId', qaController.show)
 router.post('/set/:setId/qa', qaController.create)
-// router.get('/set/:setId/qa/:qaId/edit', qaController.edit)
-// router.patch('/set/:setId/qa/:qaId', qaController.update)
-// router.delete('/set/:setId/qa/:qaId', qaController.delete)
+router.put('/set/:setId/qa/:qaId', qaController.update)
+router.delete('/set/:setId/qa/:qaId', qaController.delete)
 
 
 module.exports = router
