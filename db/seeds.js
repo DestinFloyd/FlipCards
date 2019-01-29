492b5a7d2479836f064c6d20a4e9b9492b45f8df
destin floyd <destin.b.floyd@gmail.com>
2019-01-29 18:35:15 -0500

    one more seeds fix

--- db/seeds.js
+++ db/seeds.js
@@ -49,118 +49,6 @@ const quest12 = new QA({
     question: "Filter() vs find()",
     answer: "Filter() will return all array items that meet a condition vs find will return the first to meet a condition"
 })
-const quest13 = new QA({
-    question: "Im a question 1",
-    answer: "Im answer 1"
-})
-const quest14 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest15 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const quest16 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest17 = new QA({
-    question: "Im a question 1",
-    answer: "Im answer 1"
-})
-const quest18 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest19 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const ques20 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest21 = new QA({
-    question: "Im a question 1",
-    answer: "Im answer 1"
-})
-const quest22 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest23 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const quest24 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest25 = new QA({
-    question: "Im a question 1",
-    answer: "Im answer 1"
-})
-const quest26 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest27 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const quest28 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest29 = new QA({
-    question: "Im a question 1",
-    answer: "Im answer 1"
-})
-const quest30 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest41 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const quest42 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest43 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest44 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest45 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const quest46 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest47 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
-const quest48 = new QA({
-    question: "Im a question 2",
-    answer: "Im answer 2"
-})
-const quest49 = new QA({
-    question: "Im a question 3",
-    answer: "Im answer 3"
-})
-const ques50 = new QA({
-    question: "Im a question 4",
-    answer: "Im answer 4"
-})
 
 const set1 = new Set({
     name: "Fun Trivia",
@@ -175,11 +63,6 @@ const set3 = new Set({
     stack: [quest10, quest11, quest12]
 })
 
-
-//     , quest13, quest14, quest15, quest16, quest17, quest18, quest19, quest20,
-//     quest21, quest22, quest23, quest24, quest25, quest26, quest27, quest28, quest29, quest30, 
-//     quest31, quest32, quest33, quest34, quest35, quest36, quest37, quest38, quest39, quest40,
-//     quest41, quest42, quest43, quest44, quest45, quest46, quest47, quest48, quest49, quest50
 
 Set.deleteMany({})
     .then(() => QA.deleteMany({}))
